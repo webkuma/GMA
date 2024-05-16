8003932c1ea5d99a28e81e42c8c50a888daf2f25
webkuma <137060651+webkuma@users.noreply.github.com>
2024-05-16 20:13:41 +0800

    導覽列 css 修改

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -118,7 +118,7 @@ function handleAddToLocalStorage(id) {
         >
           <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
             <li class="inline-flex items-center">
-              <RouterLink to="/" class="inline-block">
+              <RouterLink to="/" class="flex items-center">
                 <a
                   href="#"
                   class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
