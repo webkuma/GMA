4706fb1b3c510782694cd9fcf45311bea3d86fc3
webkuma <137060651+webkuma@users.noreply.github.com>
2024-06-04 17:00:56 +0800

    add splide plugin

--- src/main.js
+++ src/main.js
@@ -7,12 +7,14 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import VueLazyload from 'vue-lazyload'
+import VueSplide from '@splidejs/vue-splide';
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(VueLazyload)
+app.use( VueSplide );
 
 
 app.mount('#app')
