641972ba02985db746701be87737ecdb8a3a3a47
webkuma <137060651+webkuma@users.noreply.github.com>
2024-06-12 08:16:47 +0800

    import AOS

--- src/main.js
+++ src/main.js
@@ -8,13 +8,15 @@ import App from './App.vue'
 import router from './router'
 import VueLazyload from 'vue-lazyload'
 import VueSplide from '@splidejs/vue-splide';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(VueLazyload)
-app.use( VueSplide );
-
+app.use(VueSplide);
 
 app.mount('#app')
+AOS.init();
