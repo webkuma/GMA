c0f91a9c120ccbbfacefc8279bf96753558e0c2c
webkuma <webkuma@gmail.com>
2024-07-12 16:34:37 +0800

    1. 重構頁面結構，組件模組化。（年份下拉選單、awardsData） 2. SQLite 分離：（new）fetchShortlistYear()、fetchShortlistAwardsData() 3. 新增 mitter 用以處理元件參數傳遞

--- src/db/Sqlite.js
+++ src/db/Sqlite.js
@@ -33,3 +33,43 @@ export async function fetchShortlistByIds(parms){
   );
   return res;
 }
+
+// src\views\AwardsView.vue 的子組件(YearSelect.vue) 下拉選單獲取全部年份
+export async function fetchShortlistYear() {
+  const db = await initDatabase();
+  const res = db.exec(
+    `select DISTINCT year from shortlist ORDER BY year DESC`
+  );
+  return res;
+}
+
+// src\views\AwardsView.vue 的子組件(YearAwardsData.vue) 取得年份參數的獎項名稱、得獎作品圖片
+export async function fetchShortlistAwardsData(year) {
+  const db = await initDatabase();
+  const res = db.exec(
+    `SELECT DISTINCT awards.awards, shortlist.url
+    FROM awards, shortlist
+    WHERE awards.awards = shortlist.awards
+    AND shortlist.year = ${year}
+    AND shortlist.won = 1
+    ORDER BY CASE awards.awards
+    WHEN '年度歌曲獎' THEN 1
+    WHEN '年度專輯獎' THEN 2
+    WHEN '最佳華語專輯獎' THEN 3
+    WHEN '最佳華語男歌手獎' THEN 4
+    WHEN '最佳華語女歌手獎' THEN 5
+    WHEN '最佳新人獎' THEN 6
+    WHEN '最佳台語專輯獎' THEN 7
+    WHEN '最佳台語男歌手獎' THEN 8
+    WHEN '最佳台語女歌手獎' THEN 9
+    WHEN '最佳樂團獎' THEN 10
+    WHEN '最佳演唱組合獎' THEN 11
+    WHEN '最佳作詞人獎' THEN 12
+    WHEN '最佳作曲人獎' THEN 13
+    WHEN '最佳單曲製作人獎' THEN 14
+    WHEN '最佳專輯製作人獎' THEN 15
+    ELSE 99 -- 預設值
+    END;`
+  );
+  return res;
+}
