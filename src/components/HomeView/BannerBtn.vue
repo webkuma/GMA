532318c80d610779d9e90c7a1f5cbe8ea577b975
webkuma <webkuma@gmail.com>
2024-07-19 06:29:19 +0800

    增加條件判斷，確保跳轉時有值 v-if="lastYear"

--- src/components/HomeView/BannerBtn.vue
+++ src/components/HomeView/BannerBtn.vue
@@ -1,10 +1,10 @@
 <script setup>
-import { onMounted, ref } from 'vue';
+import { onMounted, ref } from "vue";
 // router
-import { useRouter } from 'vue-router';
+import { useRouter } from "vue-router";
 const router = useRouter();
 // pinia
-import { useGetYearDataStore } from '@/stores/counter.js';
+import { useGetYearDataStore } from "@/stores/counter.js";
 const store = useGetYearDataStore();
 
 const yearData = ref(); // 取得所有年分
@@ -31,10 +31,13 @@ function updateSelectedYear(year) {
 
 <template>
   <button
+    data-aos="fade-left"
+    data-aos-easing="ease-out-cubic"
+    data-aos-duration="1000"
+    v-if="lastYear"
     @click="updateSelectedYear(lastYear)"
     type="button"
-    class="mt-12 text-[#f4e0b2] font-black bg-gradient-to-r from-yellow-500 via-yellow-600 to-yellow-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-yellow-500 dark:focus:ring-yellow-800 rounded-lg text-base px-5 py-3 text-center me-2 mb-2"
-  >
+    class="mt-12 text-[#f4e0b2] font-black bg-gradient-to-r from-yellow-500 via-yellow-600 to-yellow-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-yellow-500 dark:focus:ring-yellow-800 rounded-lg text-base px-5 py-3 text-center me-2 mb-2">
     查看最新名單
   </button>
 </template>
